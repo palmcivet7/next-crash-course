b2f9e5da219baa1b1682e7cd2c28f7faec134563
palmcivet7 <palmcivet@proton.me>
2023-03-28 13:35:27 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#NextJS Practise
+
+This project was made by following along with a Traversy Media lesson to practise Next.js
---
+++ pages/index.js
@@ -0,0 +1,36 @@
+//
+import ArticleList from "../components/ArticleList";
+import { server } from "../config/index";
+
+export default function Home({ articles }) {
+  return (
+    <div>
+      <ArticleList articles={articles} />
+    </div>
+  );
+}
+
+export const getStaticProps = async () => {
+  console.log(server);
+  const res = await fetch(`${server}/api/articles`);
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
+
+// export const getStaticProps = async () => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts?_limit=6`
+//   );
+//   const articles = await res.json();
+
+//   return {
+//     props: {
+//       articles,
+//     },
+//   };
+// };
